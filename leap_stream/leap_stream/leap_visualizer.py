d1f61685e45ebe3284fc17f93054bdae9d563a63
lbusellato <lorenzo.busellato@gmail.com>
2025-06-05 22:52:40 +0200

    Fake leap streaming, visualization and forecasting

--- leap_stream/leap_stream/leap_visualizer.py
+++ leap_stream/leap_stream/leap_visualizer.py
@@ -6,12 +6,16 @@ from visualization_msgs.msg import Marker, MarkerArray
 from jaka_interface.pose_conversions import leap_to_jaka
 from geometry_msgs.msg import Point
 import numpy as np
+from ament_index_python.packages import get_package_share_directory
+from os.path import join
 
 class LeapVisualizer(Node):
     def __init__(self):
         super().__init__('leap_visualizer')
 
-        # Subscribe to Leap Motion JSON topic
+        self.declare_parameter('fake_data', False)
+        self.fake_data = self.get_parameter('fake_data').value
+
         self.subscription = self.create_subscription(
             String, 
             '/leap/fusion', 
@@ -20,23 +24,22 @@ class LeapVisualizer(Node):
         )
         self.subscription_ = self.create_subscription(
             String,     
-            '/applications/hand_forecasting/kalman', 
+            '/applications/hand_forecasting/nn', 
             self.forecasting_callback, 
             10
         )
 
         # Publisher for visualizing hand joints as spheres in RViz
         self.marker_publisher = self.create_publisher(MarkerArray, '/leap/visualizer/current', 10)
-        self.last_marker_count = 0  # Track last marker count
 
         self.fmarker_publisher = self.create_publisher(MarkerArray, '/leap/visualizer/forecast', 10)
-        self.flast_marker_count = 0  # Track last marker count
 
         self.min_hand_confidence = 0.1
 
     def forecasting_callback(self, msg):
 
-        try:
+        try:            
+            # TODO make a trail of every future position
             forecasting_data = json.loads(msg.data)
             future_position = forecasting_data.get('future_position', [])
             
@@ -55,20 +58,20 @@ class LeapVisualizer(Node):
             self.get_logger().error("Failed to parse Forecasting JSON data.")
 
     def leap_callback(self, msg):
-        try:
-            leap_data = json.loads(msg.data)  # Parse JSON
-            hands = leap_data.get("hands", [])  # Extract hand data
-                       
+        try:                    
             marker_array = MarkerArray()
-
-            if not hands:
-                self.clear_markers(self.marker_publisher)
-                return  
 
             marker_id = 0
             self.add_marker(marker_array, marker_id, [0,0,0], "leap", "", 0.05)
             marker_id += 1
-                        
+
+            leap_data = json.loads(msg.data)  # Parse JSON
+            hands = leap_data.get("hands", [])  # Extract hand data
+
+            if not hands:
+                self.clear_markers(self.marker_publisher)
+                return  
+            
             for hand in hands:
                 if hand is None:
                     continue
@@ -94,40 +97,48 @@ class LeapVisualizer(Node):
                             dist = np.linalg.norm(np.abs(np.array(pos['prev_joint']) - np.array(palm_pos)))
                             if dist > max_dist:
                                 max_dist = dist
-                
+                                    
                     self.add_marker(marker_array, marker_id, palm_pos, "palm", hand_type, max_dist * 2, 0.3)
                     marker_id += 1
-            self.last_marker_count = marker_id  # Track how many markers were added
+                        
             self.marker_publisher.publish(marker_array)
 
         except json.JSONDecodeError:
             self.get_logger().error("Failed to parse Leap Motion JSON data.")
 
     def add_marker(self, marker_array, marker_id, pos, joint_name, hand_type, scale=0.02, alpha=1.0, rgb=None):
-        """Creates and adds a sphere marker for RViz visualization."""
-        marker = Marker()
-        marker.header.frame_id = "world"
-        marker.header.stamp = self.get_clock().now().to_msg()
-        marker.ns = "leap_hand_joints"
-        marker.id = marker_id
-        marker.type = Marker.SPHERE
-        marker.action = Marker.ADD
-        pos = leap_to_jaka(pos)/1000
-        marker.pose.position = Point(x=pos[0], y=pos[1], z=pos[2])
-        marker.pose.orientation.w = 1.0
-        marker.scale.x = scale  # Sphere size (2 cm)
-        marker.scale.y = scale
-        marker.scale.z = scale
-        marker.color.a = alpha  # Fully visible
-        if rgb is None:
-            r, g, b = [0.0, 0.0 if hand_type == "right" else 1.0, 1.0 if hand_type == "right" else 0.0]
-        else:
-            r, g, b = rgb
-        marker.color.r = float(r)
-        marker.color.g =  float(g)
-        marker.color.b =  float(b)
+        try:
+            marker = Marker()
+            marker.header.frame_id = "world"
+            marker.header.stamp = self.get_clock().now().to_msg()
+            marker.ns = "leap_hand_joints"
+            marker.id = marker_id
+            marker.type = Marker.SPHERE
+            marker.action = Marker.ADD
 
-        marker_array.markers.append(marker)
+            if not self.fake_data:
+                pos = leap_to_jaka(pos) / 1000
+
+            marker.pose.position = Point(x=float(pos[0]), y=float(pos[1]), z=float(pos[2]))
+            marker.pose.orientation.w = 1.0
+            marker.scale.x = float(scale)
+            marker.scale.y = float(scale)
+            marker.scale.z = float(scale)
+            marker.color.a = alpha
+
+            if rgb is None:
+                r, g, b = [0.0, 0.0 if hand_type == "right" else 1.0, 1.0 if hand_type == "right" else 0.0]
+            else:
+                r, g, b = rgb
+
+            marker.color.r = float(r)
+            marker.color.g = float(g)
+            marker.color.b = float(b)
+
+            marker_array.markers.append(marker)
+
+        except Exception as e:
+            self.get_logger().error(f"Exception in add_marker: {e}")
 
     def clear_markers(self, mp):
         """Clears all markers from RViz when no hand is detected."""
